7e4a020ac05e045523df5ade4b76f28d1fed4101
KevinPizza123 <herbyemaya@gmail.com>
2025-04-12 23:04:37 -0500

    actualizacion de empleados sin necesidad de asociar usuario

--- mi_sistema_inventario/empleados/models.py
+++ mi_sistema_inventario/empleados/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class PerfilEmpleado(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    #usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     numero_de_cedula = models.CharField(max_length=20, unique=True)  # Asumiendo que la cédula es única
